55c0df91ac37606d1a1016580b91420ba35ee850
Jinxk <khaoula@gmail.com>
2023-02-17 14:43:04 -0800

    1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -3,7 +3,7 @@
 #include <time.h>
 
 /**
- * main - Entry point 
+ * main - Entry point
  * Return: Always 0
  */
 int main(void)
@@ -11,9 +11,9 @@ int main(void)
 	int n;
 	int m;
 
-	srand(time(0));
-	n = rand() - RAND_MAX /2;
-	m = n % 10;
+	strand(time(0));
+	n = rand() - RAND_MAX / 2;
+	m = n %10;
 
 	if (m > 5)
 	{
