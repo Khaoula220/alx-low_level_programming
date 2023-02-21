881565937a68fd938fe8d129d4c591bbef029625
Jinxk <khaoula@gmail.com>
2023-02-21 13:29:01 -0800

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,13 +2,13 @@
 /**
  * main - prints the alphabet, in lowercase, followed by a new line
  */
-void print_alphabet(void);
+void print_alphabet(void)
 {
 	char la;
 
 	for (la = 'a'; la <= 'z'; la++)
-
 		_putchar(la);
+
 
 	_putchar('\n');
 }
